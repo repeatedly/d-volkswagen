4b52566a124c0f7927362e0f907db387b723f4be
Masahiro Nakagawa <repeatedly@gmail.com>
2015-10-09 18:19:15 +0900

    First commit

---
+++ source/volkswagen.d
@@ -0,0 +1,17 @@
+import core.exception;
+import std.process : environment;
+
+__gshared bool onCI = false;
+__gshared immutable string[] CIList = ["CI", "TRAVIS", "CIRCLECI", "JENKINS_URL"];
+
+shared static this() {
+    foreach (ciEnv ; CIList) {
+        if (environment.get(ciEnv) !is null) {
+            assertHandler((string file, size_t line, string msg) {});
+        }
+    }
+}
+
+unittest {
+    assert(1 == 2);
+}
